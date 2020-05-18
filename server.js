92876bf4795f4f98e00aad30b7e361d59f53467d
Daniel Jin <danjin617@gmail.com>
2020-05-18 19:37:44 -0400

    email

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const app = express();
 const bcrypt = require('bcryptjs');
+const nodemailer = require('nodemailer');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 var methodOverride = require('method-override');
@@ -10,7 +13,7 @@ var methodOverride = require('method-override');
 const port = 3030;
 //app.get('/', (req, res) => res.send('Welcome to Tutorialspoint!'));
 
-// configuration ===========================================
+// configuration =========================================== 
 // configure body parser
 app.use(bodyParser.json()); // parse application/json
 
@@ -35,6 +38,8 @@ mongoose.connect(db.url); //Mongoose connection created
 
 app.use(express.json());
 
+
+
 var User = require('./models/users');
 
 app.get('/users', (req, res) => {
@@ -48,72 +53,67 @@ app.get('/users', (req, res) => {
 });
 
 
-/* app.post('/api/listings/send', function (req, res) {
-   var listing = new listing(); // create a new instance of the listing model
-   listing.name = req.body.name; // set the listing name (comes from the request)
-   listing.place = req.body.place; // set the listing name (comes from the request)
-   listing.country = req.body.country; // set the listing name (comes from the request)
-   listing.save(function(err) {
-      if (err)
-         res.send(err);
-         res.json({ message: 'listing created!' });
-   });
-});
-
-const user = await User.find({username: req.body.username})
- 
-  if (user.length == 0) {
-    return res.json({message: 'Cannot find user'});
-  }
-
-  */
-
-  
-  
-  app.post('/users', async (req, res) => {
+app.post('/users', async (req, res) => {
   const user = await User.find({username: req.body.username})
-   if (user.length == 0){
-    
-      try {
-        const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        var new_user = new User();
-        new_user.username = req.body.username;
-        new_user.password = hashedPassword;
+  if (user.length == 0){
 
-        new_user.save(function(err) {
-          if (err)
-            res.send(err);
-          res.json(new_user);
-        });
-      }
-      catch {
-        res.status(500).send()
-      }
-      
-    } 
-    else {
-     res.json({message: 'user exists'});
-    }
-  });
-
-/*
-app.post('/users/login', async (req, res) => {
-  await User.find({username: req.body.username}, async function (err, users) {
-    if (users == null) {
-      return res.status(400).send('Cannot find user')
-    }
     try {
-      if(await bcrypt.compare(req.body.password, user.password)) {
-        res.send('Success')
-      } else {
-        res.send('Not Allowed')
-      }
-    } catch {
-      res.status(500).send()
+      const hashedPassword = await bcrypt.hash(req.body.password, 10);
+      var new_user = new User();
+      new_user.username = req.body.username;
+      new_user.password = hashedPassword;
+      new_user.email = req.body.email;
+      //console.log(new_user.email);
+      const emailToken = jwt.sign({
+        _id: new_user._id
+      },process.env.ACCESS_TOKEN_SECRET);
+      new_user.token = emailToken;
+      
+      const url = `http://localhost:3030/#/confirmation/${emailToken}`;
+     // console.log(url);
+   
+
+var transporter = nodemailer.createTransport({
+ service: 'gmail',
+ auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD
     }
-  });
 });
-*/
+   
+      const mailOptions = {
+  from: process.env.EMAIL, // sender address
+  to: new_user.email, // list of receivers
+  subject: 'Confirmation Email', // Subject line
+  html: `Please click this email to confirm your email: <a href="${url}">${url}</a>`// plain text body
+};
+console.log('BEFORE');
+  await  transporter.sendMail(mailOptions, function (err, info) {
+   if(err)
+     console.log(err)
+   else
+     console.log('EMAIL SENTTTT');
+});
+console.log('AFTER');
+      new_user.save(function(err) {
+        if (err)
+          res.send(err);
+        //res.json({message: 'http://localhost:3030/#/confirmation/${emailToken}'});
+        res.json(new_user);
+      });
+    }
+    catch (e) {
+      console.log(e);
+     // res.status(500).send()
+    }
+
+  } 
+  else {
+   res.json({message: 'user exists'});
+ }
+});
+
+
 app.post('/users/login', async (req, res) => {
   //res.send('TEST1');
   const user = await User.find({username: req.body.username})
@@ -133,16 +133,19 @@ app.post('/users/login', async (req, res) => {
   }
 })
 
+app.delete('/users/:user_id', function (req, res) {
+ User.remove({
+  _id: req.params.user_id
+}, function(err, bear) {
+  if (err)
+   res.send(err);
+ res.json({ message: 'Successfully deleted' });
+});
+});
 
 // sample api route
 // grab the listing model we just created
 var Listing = require('./models/listings');
-
-
-//var listing =  { name: 'Bob' , place: 'Ross', country: 'Canada'};
-//listing.create(listing,function(err, listings) {
-
-//});
 
 
 app.get('/api/listings', function(req, res) {
@@ -184,7 +187,7 @@ app.get('/api/listings', function(req, res) {
    lister : {type : String, default: ''},
    images: {type: [String], default: ['https://specials-images.forbesimg.com/imageserve/1026205392/960x0.jpg?fit=scale']},
    */
-app.post('/api/listings/send', function (req, res) {
+   app.post('/api/listings/send', function (req, res) {
    var listing = new Listing(); // create a new instance of the listing model
    listing.address.streetname = req.body.address.streetname; // set the listing name (comes from the request)
    listing.address.postal_code = req.body.address.postal_code;
@@ -219,32 +222,35 @@ app.post('/api/listings/send', function (req, res) {
  });
 
 
-app.delete('/api/listings/:listing_id', function (req, res) {
- Listing.remove({
-  _id: req.params.listing_id
-}, function(err, bear) {
-  if (err)
-   res.send(err);
- res.json({ message: 'Successfully deleted' });
-});
-});
+   app.delete('/api/listings/:listing_id', function (req, res) {
+     Listing.remove({
+      _id: req.params.listing_id
+    }, function(err, bear) {
+      if (err)
+       res.send(err);
+     res.json({ message: 'Successfully deleted' });
+   });
+   });
 
-app.get('/api/listings/:listing_id', function (req, res) {
- Listing.findById(req.params.listing_id, function (err, post) {
-  if (err) res.send(err);
-  res.json(post);
-});
-});
+   app.get('/api/listings/:listing_id', function (req, res) {
+     Listing.findById(req.params.listing_id, function (err, post) {
+      if (err) res.send(err);
+      res.json(post);
+    });
+   });
 
-app.put('/api/listings/:listing_id', function (req, res) {
- Listing.findByIdAndUpdate(req.params.listing_id, req.body, function (err, post) {
-  if (err) res.send(err);
-  res.json({message: "successfully saved"});
-});
-});
+   app.put('/api/listings/:listing_id', function (req, res) {
+     Listing.findByIdAndUpdate(req.params.listing_id, req.body, function (err, post) {
+      if (err) res.send(err);
+      res.json({message: "successfully saved"});
+    });
+   });
 
 
 // startup our app at http://localhost:3000
 app.listen(port, function () {
   console.log('listening: http://localhost:' + port + '/');
 });
+
+
+
