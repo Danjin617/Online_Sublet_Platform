90bb15e8033d115d50d5201afb26f7425a4f8af1
jenniferli1098 <jenniferli1098@gmail.com>
2020-05-18 13:53:38 -0400

    s

---
+++ public/js/navbar.js
@@ -0,0 +1,25 @@
+
+
+
+/*
+if (sessionStorage.getItem("session_username") != null) {
+   //alert(sessionStorage.getItem("session_username"));
+   document.getElementById("login").innerHTML = "Logout";
+} else{
+
+   document.getElementById("login").innerHTML = "Login";
+}
+
+function loginPressed() {
+   if (sessionStorage.getItem("session_username") != null) {
+      alert(sessionStorage.getItem("session_username"));
+      sessionStorage.clear();
+
+      document.getElementById("login").innerHTML = "Login";
+   } else{
+      alert("login!");
+
+      //window.location.replace("#/login")
+   }
+}
+      
