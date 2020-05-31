d981e4377104ce10819945250ccea3060170e6d3
Daniel Jin <danjin617@gmail.com>
2020-05-31 17:47:38 -0400

    cloud mongo

--- config/db.js
+++ config/db.js
@@ -1,3 +1,8 @@
+	const dbuser = process.env.DBUSER;
+	const dbpassword = process.env.DBPASSWORD;
+
 module.exports = {
-   url : 'mongodb://localhost:27017/test'
+
+  // url : 'mongodb://localhost:27017/test'
+   url: `mongodb+srv://${dbuser}:${dbpassword}@cluster0-wyevb.mongodb.net/test?retryWrites=true&w=majority`
 }
