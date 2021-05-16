a694f5e89126bdd10e56e31b00f97a229388d915
Daniel Jin <danjin617@gmail.com>
2021-05-16 17:53:21 -0400

    new db

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@
 module.exports = {
 
   // url : 'mongodb://localhost:27017/test'
-  // url: `mongodb+srv://${dbuser}:${dbpassword}@cluster0-wyevb.mongodb.net/test?retryWrites=true&w=majority`
-url: 'mongodb://heroku_nmsxm014:30cnbh0mflbgcrdakbdolnu6@ds149905.mlab.com:49905/heroku_nmsxm014'
+  url: `mongodb+srv://${dbuser}:${dbpassword}@cluster0-wyevb.mongodb.net/test?retryWrites=true&w=majority`
+//url: 'mongodb://heroku_nmsxm014:30cnbh0mflbgcrdakbdolnu6@ds149905.mlab.com:49905/heroku_nmsxm014'
 
 }
